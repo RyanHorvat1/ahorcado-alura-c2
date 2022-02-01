cfec23a238377063ced977b2ad3527012478a30b
Ryan Horvat <ryanhorvat.oracle@outlook.com>
2022-02-01 13:57:55 +0000

    Primera carga de archivos, funciones extra interactuando OK, Falta desarrollo de las demás funcuones y mejora del CSS

---
+++ Readme.md
@@ -0,0 +1,9 @@
+[ES]
+Agradecimentos especiales a:
+
+Darrell Flood por haber hecho pública la fuente Chalktastic para uso personal. Su perfil en FontSpace: https://www.fontspace.com/darrell-flood
+
+[EN]
+Special thanks to:
+
+Darrell Flood for made public the Chalktastic font for personal use.  His profile on FontSpace at: https://www.fontspace.com/darrell-flood
---
+++ js/script.js
@@ -0,0 +1,78 @@
+const btnStart = document.getElementById("btn-start");
+const addedWords = document.getElementById("input-add-words");
+const startZone = document.querySelector("#start-settings");
+const predWords = document.getElementById("use-predetermined-words");
+const ownWords = document.getElementById("use-own-words");
+const errorPalabras = document.getElementById("error-palabras");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+context.fillStyle = "#333";
+context.fillRect(0, 0, 1 * canvas.width, 1 * canvas.height);
+const playing = document.querySelector(".mar-ls");
+const inputTry = document.getElementById("input-try");
+const btnManGroup = document.getElementById("manual-buttons");
+const scndRestart = document.querySelector(".scnd");
+const autValidation = document.getElementById("automatic-validation");
+const manValidation = document.getElementById("manual-validation");
+
+function checkInitSelection() {
+    var o = ownWords.checked;
+    var p = predWords.checked;
+    if ((o && p) || (o && !p)) {
+        addedWords.style.display = "block";
+        btnStart.style.display = "block";
+        btnStart.style.transform = "translateX(0%)";
+        errorPalabras.style.display = "none";
+    } else if (!o && p) {
+        addedWords.style.display = "none";
+        btnStart.style.display = "block";
+        btnStart.style.transform = "translateX(90%)";
+        errorPalabras.style.display = "none";
+    } else {
+        addedWords.style.display = "none";
+        btnStart.style.display = "none";
+        errorPalabras.style.display = "block";
+    }
+}
+
+predWords.addEventListener("change", function() {
+    checkInitSelection();
+});
+
+ownWords.addEventListener("change", function() {
+    checkInitSelection();
+});
+
+btnStart.addEventListener("click", function() {
+    canvas.style.display = "block";
+    playing.style.display = "block";
+    btnStart.style.display = "none";
+    addedWords.style.display = "none";
+    startZone.style.display = "none";
+});
+
+manValidation.addEventListener("change", function() {
+    if (manValidation.checked) {
+        autValidation.checked = false;
+        inputTry.placeholder = "Ingrese una letra o una palabra para comprobar.";
+        btnManGroup.style.display = "block";
+        scndRestart.style.display = "none";
+    } else {
+        autValidation.checked = true;
+        btnManGroup.style.display = "none";
+        scndRestart.style.display = "block";
+    }
+})
+
+autValidation.addEventListener("change", function() {
+    if (autValidation.checked) {
+        manValidation.checked = false;
+        inputTry.placeholder = "Ingrese letras para comprobar.";
+        btnManGroup.style.display = "none";
+        scndRestart.style.display = "block";
+    } else {
+        manValidation.checked = true;
+        btnManGroup.style.display = "block";
+        scndRestart.style.display = "none";
+    }
+})
